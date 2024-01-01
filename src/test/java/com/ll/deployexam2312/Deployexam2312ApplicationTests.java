1a0daa44d8fd6104d0674f67a0dd94a3c1e0b49e
tallguy188 <cksghd4220@naver.com>
2024-01-01 23:31:33 +0900

    test,prod

--- src/test/java/com/ll/deployexam2312/Deployexam2312ApplicationTests.java
+++ src/test/java/com/ll/deployexam2312/Deployexam2312ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.deployexam2312;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Deployexam2312ApplicationTests {
 
     @Test
